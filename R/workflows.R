1780ff7c931c8d0b17b9aa7b3e956b7ff2330776
Raphael Guyot <rguyot@sanqualis.com>
2024-10-27 17:37:21 +0900

    small fix again related to parsing multiple arguments.

--- R/workflows.R
+++ R/workflows.R
@@ -806,9 +806,9 @@ set_processing_skill <- function(workflow_obj, processing_skill, ...) {
   
   # Capture additional arguments - since they are part of list, we get the list first element
   if (length(list(...)) > 0) {
-  additional_args <- list(...)[[1]]
+  additional_args <- list(...)
   } else { additional_args <- list() }
-  
+
   processing_skill_source <- glue::glue("{processing_skill}.txt")
   inst_dir <- system.file(package = 'aiworkflow') 
   skills <- list_processing_skills()
