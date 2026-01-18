e7e3e342ad8862abc927944047de43a4e40771e0
Raphael Guyot <rguyot@sanqualis.com>
2026-01-18 23:33:31 +0900

    beginning of work on llamacpp server

--- R/llamacpp.R
+++ R/llamacpp.R
@@ -25,3 +25,69 @@ test_llamacpp_connection <- function(ip_ad="127.0.0.1", port="8080") {
   error=function(error) {print(error)})
   
 }
+
+
+
+get_llamacpp_connection <- function(ip_ad="127.0.0.1", port="8080") {
+  
+    return(list(llamacpp_server_ip=ip_ad, llamacpp_server_port=port))
+
+}
+
+
+
+
+get_llamacpp_model_info <- function(llamacpp_connection, model) {
+  
+  url <- glue::glue("{llamacpp_connection$llamacpp_server_ip}:{llamacpp_connection$llamacpp_server_port}/api/show")
+  tryCatch({
+    req <- httr2::request(url) 
+    result <- req |> 
+      httr2::req_body_json(data = list(
+        name = model)) |> 
+      httr2::req_perform() 
+    if (result$status_code=="200") { 
+      return(httr2::resp_body_json(result) |> convert_llamacpp_model_info_response_to_tibble())
+    }
+  },
+  error=function(error) {print(error)})
+  
+}
+
+get_list_llamacpp_models <- function(ollama_connection) {
+  
+  url <- glue::glue("{llamacpp_connection$llamacpp_server_ip}:{llamacpp_connection$llamacpp_server_port}/api/tags")
+  tryCatch({
+    req <- httr2::request(url) 
+    result <- req |>
+      httr2::req_perform() 
+    if (result$status_code=="200") { 
+      return(httr2::resp_body_json(result) |> convert_ollama_tags_response_to_tibble())
+    }
+  },
+  error=function(error) {print(error)})
+}
+
+
+convert_llamacpp_tags_response_to_tibble <- function(llamacpp_response) {
+  
+  # to implement
+  
+}
+
+
+convert_llamacpp_model_info_response_to_tibble <- function(llamacpp_response) {
+  
+  temp <- llamacpp_response
+  
+  context_length <- temp$model_info$llama.context_length
+  template <- temp$template |> unique()
+  parent_model <- temp$details$parent_model
+  format <- temp$details$format
+  parameter_size <- temp$details$parameter_size
+  quantization_level <- temp$details$quantization_level
+  
+  res <- tibble::tibble(context_length, template, parent_model, format, parameter_size, quantization_level)
+  
+  return(res)
+}
